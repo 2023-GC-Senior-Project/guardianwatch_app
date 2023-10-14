83ccc8eb4818dbfeea26ad8beb1338312cb787f0
Choi Jee Woo <80667141+cjw020607@users.noreply.github.com>
2023-10-14 12:25:43 +0900

    Update child list

--- app/src/main/java/com/example/guardianwatch/ChildListActivity.java
+++ app/src/main/java/com/example/guardianwatch/ChildListActivity.java
@@ -1,8 +1,16 @@
 package com.example.guardianwatch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ChildListActivity extends AppCompatActivity {
 
@@ -10,5 +18,27 @@ public class ChildListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child_list);
+
+        //===== 테스트를 위한 더미 데이터 생성 ===================
+        ArrayList<String> testDataSet = new ArrayList<>();
+        for (int i = 0; i<20; i++) {
+            testDataSet.add("TEST DATA" + i);
+        }
+        //========================================================
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        //--- LayoutManager는 아래 3가지중 하나를 선택하여 사용 ---
+        // 1) LinearLayoutManager()
+        // 2) GridLayoutManager()
+        // 3) StaggeredGridLayoutManager()
+        //---------------------------------------------------------
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager((Context) this);
+        recyclerView.setLayoutManager(linearLayoutManager);  // LayoutManager 설정
+
+        CustomAdapter customAdapter = new CustomAdapter(testDataSet);
+        recyclerView.setAdapter(customAdapter); // 어댑터 설정
+
     }
+
 }
