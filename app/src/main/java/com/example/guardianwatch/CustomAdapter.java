4b1ffe4d2953cee008e2aa46b07d934dbd96a736
Choi Jee Woo <80667141+cjw020607@users.noreply.github.com>
2023-10-15 18:16:15 +0900

    Update child list

--- app/src/main/java/com/example/guardianwatch/CustomAdapter.java
+++ app/src/main/java/com/example/guardianwatch/CustomAdapter.java
@@ -1,8 +1,13 @@
 package com.example.guardianwatch;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,21 +18,55 @@ import java.util.ArrayList;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
 
-    private ArrayList<ChildData> localDataSet;
+    private static ArrayList<ChildData> localDataSet;
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView nameTextView, birthDateTextView, placeTextView;
         private ImageView profileImageView;
+        private ImageView closeImageView;
+        private TextView editTextView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             nameTextView = itemView.findViewById(R.id.name);
             birthDateTextView = itemView.findViewById(R.id.birthDate);
             placeTextView = itemView.findViewById(R.id.place);
-            profileImageView = itemView.findViewById(R.id.profile_image);  // 추가
+            profileImageView = itemView.findViewById(R.id.profile_image);
+            closeImageView = itemView.findViewById(R.id.close);
+            editTextView = itemView.findViewById(R.id.edit);
+            // X 누를 시에 확인 메시지 후 아이 목록에서 삭제
+            closeImageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (getAdapterPosition() != RecyclerView.NO_POSITION) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                        builder.setTitle("삭제 확인");
+                        builder.setMessage("정말로 이 항목을 삭제하시겠습니까?");
+                        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                localDataSet.remove(getAdapterPosition());
+                                notifyItemRemoved(getAdapterPosition());
+                            }
+                        });
+                        builder.setNegativeButton("아니오", null);
+                        builder.show();
+                    }
+                }
+            });
+            //편집 누를 시에 아이 편집 페이지로 이동
+            editTextView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Context context = view.getContext();
+                    Intent intent = new Intent(context, EditProfileActivity.class);
+                    context.startActivity(intent);
+                }
+            });
 
         }
     }
+
 
     public CustomAdapter(ArrayList<ChildData> dataSet) {
         localDataSet = dataSet;
